b4167880813df31edb032fa3dcdfdcf5b3e09e01
German Perov <ggmusya@gmail.com>
2025-09-24 12:25:17 +0300

    Use TEA_DEPS_PATH for cmake/grpc.cmake

--- cmake/grpc.cmake
+++ cmake/grpc.cmake
@@ -75,14 +75,14 @@ elseif(GRPC_FETCHCONTENT)
 else()
   # This branch assumes that gRPC and all its dependencies are already installed
   # on this system, so they can be located by find_package().
-  find_package(absl CONFIG REQUIRED)
+  find_package(absl CONFIG REQUIRED ${TEA_DEPS_PATH})
   message(STATUS "Using absl ${absl_VERSION}")
 
   # Find Protobuf installation Looks for protobuf-config.cmake file installed by
   # Protobuf's cmake installation.
 
   set(protobuf_MODULE_COMPATIBLE TRUE)
-  find_package(Protobuf CONFIG REQUIRED)
+  find_package(Protobuf CONFIG REQUIRED ${TEA_DEPS_PATH})
   message(STATUS "Using protobuf ${Protobuf_VERSION}")
 
   set(_PROTOBUF_LIBPROTOBUF protobuf::libprotobuf)
@@ -95,7 +95,7 @@ else()
 
   # Find gRPC installation Looks for gRPCConfig.cmake file installed by gRPC's
   # cmake installation.
-  find_package(gRPC CONFIG REQUIRED)
+  find_package(gRPC CONFIG REQUIRED ${TEA_DEPS_PATH})
   message(STATUS "Using gRPC ${gRPC_VERSION}")
 
   set(_GRPC_GRPCPP gRPC::grpc++)
