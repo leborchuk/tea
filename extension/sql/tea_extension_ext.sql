d4bf1148ab9bad3e6daaf5330180d39bbf4e5741
Leonid Borchuk <xifos@yandex-team.ru>
2026-07-29 13:44:51 +0300

    Fix table names - reflect actual tests

--- extension/sql/tea_extension_ext.sql
+++ extension/sql/tea_extension_ext.sql
@@ -2,11 +2,11 @@
 CREATE EXTENSION IF NOT EXISTS tea;
 -- end_ignore
 
-CREATE READABLE EXTERNAL TABLE tea_ext_empty_probe_ext (a bigint)
+CREATE READABLE EXTERNAL TABLE tea_ext_empty_probe (a bigint)
 LOCATION ('tea://special://empty')
 FORMAT 'custom' (formatter = tea_import);
 
 SELECT count(*)::int AS rows_read
-FROM tea_fdw_empty_probe_ext;
+FROM tea_ext_empty_probe;
 
-DROP FOREIGN TABLE tea_fdw_empty_probe_ext;
+DROP FOREIGN TABLE tea_ext_empty_probe;
--- extension/sql/tea_extension_fdw.sql
+++ extension/sql/tea_extension_fdw.sql
@@ -2,11 +2,11 @@
 CREATE EXTENSION IF NOT EXISTS tea;
 -- end_ignore
 
-CREATE FOREIGN TABLE tea_fdw_empty_probe_fdw (a bigint)
+CREATE FOREIGN TABLE tea_fdw_empty_probe (a bigint)
 SERVER tea_server
 OPTIONS (location 'tea://special://empty');
 
 SELECT count(*)::int AS rows_read
-FROM tea_fdw_empty_probe_fdw;
+FROM tea_fdw_empty_probe;
 
-DROP FOREIGN TABLE tea_fdw_empty_probe_fdw;
+DROP FOREIGN TABLE tea_fdw_empty_probe;
